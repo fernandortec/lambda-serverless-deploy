10daeeb48a7bd2fe6553f0eacd1ca958eef44fb7
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2021-09-06 16:54:26 -0300

    changes to CI, staging deploy

--- src/handler.spec.ts
+++ src/handler.spec.ts
@@ -1,5 +1,5 @@
 describe("the handler function", () => {
-    it("should make a extremely simple test", () => {
-        expect(2 + 2).toBe(4)
-    })
+  it("should make a extremely simple test", () => {
+    expect(2 + 2).toBe(4)
+  })
 })
