937cbe781671ed3d20e19d0d01b1dc33e7822579
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2021-09-06 15:42:02 -0300

    dev stage added to CI

--- src/handler.ts
+++ src/handler.ts
@@ -5,9 +5,8 @@ import {
 export const lambdaHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const queries = JSON.stringify(event.queryStringParameters);
   return {
     statusCode: 200,
-    body: `Queries: ${queries}, '<><><><><><>`
+    body: `Event: ${event}`
   }
 }
